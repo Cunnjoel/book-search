d3a59f74746049b7b97a2243c1eb8e4b3eb6336c
Joel C <cunningham.joel84@gmail.com>
2021-07-10 00:32:07 -0500

    pushing most recent

--- client/src/components/SaveCard.js
+++ client/src/components/SaveCard.js
@@ -1,53 +1,54 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 import API from "../utils/API"
 
 function SaveCard(props) {
+
     const [books, setBooks] = useState([])
     useEffect(() => {
         loadBooks()
-      }, [])
+    }, books)
 
-      function loadBooks() {
+    function loadBooks() {
         axios.get("/api/books")
-          .then(res => 
-            setBooks(res.data)
-          )
-          .catch(err => console.log(err));
-      };
-      
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
     function deleteBook(_id) {
         axios.delete("/api/books/" + _id)
-          .then(res => loadBooks())
-          .catch(err => console.log(err));
-      }
+            .then(res => loadBooks())
+            .catch(err => console.log(err));
+    }
 
-  return (
-    <div className="card">
-        <Button variant="outline-dark" href={props.link}>View</Button>
-        <Button variant="outline-dark" onClick={() => deleteBook(props.id)}>Delete</Button>
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Title:</strong>{props.title}
-          </li>
-          <li>
-            <strong>Author:</strong> {props.author}
-          </li>
-          <li>
-            <strong>Description:</strong> {props.description}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
-    </div>
-  );
+    return (
+        <div className="card">
+            <Button variant="outline-dark" href={props.link}>View</Button>
+            <Button variant="outline-dark" onClick={() => deleteBook(props.id)}>Delete</Button>
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Title:</strong>{props.title}
+                    </li>
+                    <li>
+                        <strong>Author:</strong> {props.author}
+                    </li>
+                    <li>
+                        <strong>Description:</strong> {props.description}
+                    </li>
+                </ul>
+            </div>
+            <span onClick={() => props.removeFriend(props.id)} className="remove">
+                𝘅
+            </span>
+        </div>
+    );
 }
 
 export default SaveCard;
